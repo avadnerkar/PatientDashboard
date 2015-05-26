a603e84ae806efd88ab6a67bec3498cc13f0c7ba
avadnerkar <avadnerkar@gmail.com>
2015-05-26 18:59:16 -0400

    Added datepicker dialogs

--- app/src/main/java/com/physiotherapy/mcgill/patientmonitoring/NewPatientFormActivity.java
+++ app/src/main/java/com/physiotherapy/mcgill/patientmonitoring/NewPatientFormActivity.java
@@ -1,19 +1,37 @@
 package com.physiotherapy.mcgill.patientmonitoring;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 public class NewPatientFormActivity extends ActionBarActivity {
+    TextView txtDateOT;
+    TextView txtDateSwallow;
+    TextView txtDatePT;
+    TextView txtDateSLT;
+    public String dateFirstOTString = "";
+    public String dateFirstSwallowString = "";
+    public String dateFirstPTString = "";
+    public String dateFirstSLTString = "";
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_patient_form);
+        txtDateOT = (TextView) findViewById(R.id.textDateFirstOT);
+        txtDateSwallow = (TextView) findViewById(R.id.textDateFirstSwallow);
+        txtDatePT = (TextView) findViewById(R.id.textDateFirstPT);
+        txtDateSLT = (TextView) findViewById(R.id.textDateFirstSLT);
     }
 
     @Override
@@ -49,7 +67,7 @@ public class NewPatientFormActivity extends ActionBarActivity {
             int day = datePicker.getDayOfMonth();
             int month = datePicker.getMonth();
             int year = datePicker.getYear();
-            String admissionDate = year + "-" + month + "-" + day;
+            String admissionDate = year + "-" + (month+1) + "-" + day;
 
             long newID = MainActivity.myDb.insertRowPatient(firstName, lastName, hospitalId, admissionDate);
 
@@ -64,4 +82,93 @@ public class NewPatientFormActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void selectDateOTFunc(View view){
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // Launch Date Picker Dialog
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // Display Selected date in textbox
+                        dateFirstOTString = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                        txtDateOT.setText(dateFirstOTString);
+
+                    }
+                }, mYear, mMonth, mDay);
+        dpd.show();
+    }
+
+    public void selectDateSwallowFunc(View view){
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // Launch Date Picker Dialog
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year,
+                                  int monthOfYear, int dayOfMonth) {
+                // Display Selected date in textbox
+                dateFirstSwallowString = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                txtDateSwallow.setText(dateFirstSwallowString);
+
+            }
+        }, mYear, mMonth, mDay);
+        dpd.show();
+    }
+
+    public void selectDatePTFunc(View view){
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // Launch Date Picker Dialog
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year,
+                                  int monthOfYear, int dayOfMonth) {
+                // Display Selected date in textbox
+                dateFirstPTString = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                txtDatePT.setText(dateFirstPTString);
+
+            }
+        }, mYear, mMonth, mDay);
+        dpd.show();
+    }
+
+    public void selectDateSLTFunc(View view){
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // Launch Date Picker Dialog
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year,
+                                  int monthOfYear, int dayOfMonth) {
+                // Display Selected date in textbox
+                dateFirstSLTString = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                txtDateSLT.setText(dateFirstSLTString);
+
+            }
+        }, mYear, mMonth, mDay);
+        dpd.show();
+    }
+
 }
